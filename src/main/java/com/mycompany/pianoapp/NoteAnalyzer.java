49549a153f1448a9b6c2d54422da00cd6a32cbd2
shdonas <BlackHole7473>
2020-05-01 14:43:06 -0400

    bug fixed

--- src/main/java/com/mycompany/pianoapp/NoteAnalyzer.java
+++ src/main/java/com/mycompany/pianoapp/NoteAnalyzer.java
@@ -1,16 +1,7 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package com.mycompany.pianoapp;
 
 import org.apache.commons.lang3.ArrayUtils;
 
-/**
- *
- * @author Shakhawat Hossain <shossain_psu@yahoo.com>
- */
 public class NoteAnalyzer {
 
     int noteLength;
